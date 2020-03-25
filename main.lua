642434b829c7ddaa655c8d5a6f0f284745fe72d6
bakudas <bakudas@gmail.com>
2020-03-24 23:37:43 -0300

    create main.lua to start the project

---
+++ main.lua
@@ -0,0 +1,24 @@
+
+
+pad_left = {
+    width = 10,
+    height = 30,
+    x = 30,
+    y = 40,
+    speed = 50,
+}
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.print("hello vaca!")
+
+    love.graphics.rectangle("fill", pad_left.x, pad_left.y, pad_left.width, pad_left.height)
+
+end
